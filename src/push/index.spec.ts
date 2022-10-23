8101bf9b5f9c7eb621774269e09133f4d958f37e
Takanori Oishi <takanori0014@live.jp>
2022-10-23 20:12:50 +0900

    fix tests

--- src/push/index.spec.ts
+++ src/push/index.spec.ts
@@ -67,19 +67,19 @@ describe.each(pushEvents)("index", (event) => {
     mocked(core.setFailed).mockImplementation((m) => m)
   })
 
-  test("main resolve with the message", async () => {
+  test("resolve with the message", async () => {
     const postCommentsResponse: Response = basePostCommentsResponse
     mocked(postComments).mockImplementation(() =>
       Promise.resolve([postCommentsResponse])
     )
-    await expect(push({ ...configs, event })).resolves.toEqual(
+    await expect(push({ ...configs, event })).resolves.toStrictEqual(
       "正常に送信しました。"
     )
     expect(core.info).toHaveBeenCalledTimes(1)
     expect(core.info).toHaveBeenCalledWith(message)
   })
 
-  test("main resolve with the message when commits with fix_keyword", async () => {
+  test("resolve with the message when commits with fix_keyword", async () => {
     const postCommentsResponse: Response = {
       ...basePostCommentsResponse,
       isFix: true,
@@ -87,7 +87,7 @@ describe.each(pushEvents)("index", (event) => {
     mocked(postComments).mockImplementation(() =>
       Promise.resolve([postCommentsResponse])
     )
-    await expect(push({ ...configs, event })).resolves.toEqual(
+    await expect(push({ ...configs, event })).resolves.toStrictEqual(
       "正常に送信しました。"
     )
     expect(core.info).toHaveBeenCalledTimes(2)
@@ -95,7 +95,7 @@ describe.each(pushEvents)("index", (event) => {
     expect(core.info).toHaveBeenCalledWith(`${issueKey}を処理済みにしました。`)
   })
 
-  test("main resolve with the message when commits with close_keyword", async () => {
+  test("resolve with the message when commits with close_keyword", async () => {
     const postCommentsResponse: Response = {
       ...basePostCommentsResponse,
       isClose: true,
@@ -103,7 +103,7 @@ describe.each(pushEvents)("index", (event) => {
     mocked(postComments).mockImplementation(() =>
       Promise.resolve([postCommentsResponse])
     )
-    await expect(push({ ...configs, event })).resolves.toEqual(
+    await expect(push({ ...configs, event })).resolves.toStrictEqual(
       "正常に送信しました。"
     )
     expect(core.info).toHaveBeenCalledTimes(2)
@@ -111,18 +111,18 @@ describe.each(pushEvents)("index", (event) => {
     expect(core.info).toHaveBeenCalledWith(`${issueKey}を完了にしました。`)
   })
 
-  test("main not continue and resolve processing when commits without issueKey", async () => {
+  test("not continue and resolve processing when commits without issueKey", async () => {
     mocked(parseCommits).mockImplementation(() => ({
       parsedCommits: null,
     }))
-    await expect(push({ ...configs, event })).resolves.toEqual(
+    await expect(push({ ...configs, event })).resolves.toStrictEqual(
       "課題キーのついたコミットが1件も見つかりませんでした。"
     )
   })
 
-  test("main not continue and resolve processing when the event.ref is invalid", async () => {
+  test("not continue and resolve processing when the event.ref is invalid", async () => {
     mocked(parseRef).mockImplementation(() => undefined)
-    await expect(push({ ...configs, event })).resolves.toEqual(
+    await expect(push({ ...configs, event })).resolves.toStrictEqual(
       "Git referenceの解析に失敗しました。"
     )
   })
