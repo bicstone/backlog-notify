519a49c36717bcdcc425adb70fd51eff3df77e7d
Oishi Takanori <takanori0014@live.jp>
2023-12-29 20:16:19 +0900

    refactor: default value (#150)

--- src/main/getConfigs.ts
+++ src/main/getConfigs.ts
@@ -17,6 +17,58 @@ export type Configs = {
   closeStatusId: string
 }
 
+export type RequiredConfigKeys =
+  | "projectKey"
+  | "apiHost"
+  | "apiKey"
+  | "githubEventPath"
+
+export const defaultConfigs: Omit<Configs, RequiredConfigKeys> = {
+  fixKeywords: ["#fix", "#fixes", "#fixed"],
+  closeKeywords: ["#close", "#closes", "#closed"],
+  pushCommentTemplate:
+    "<%= commits[0].author.name %>さんが[<%= ref.name %>](<%= ref.url %>)にプッシュしました" +
+    "\n" +
+    "<% commits.forEach(commit=>{ %>" +
+    "\n" +
+    "+ [<%= commit.comment %>](<%= commit.url %>) (<% print(commit.id.slice(0, 7)) %>)" +
+    "<% }); %>",
+  prOpenedCommentTemplate:
+    "<%= sender.login %>さんがプルリクエストを作成しました" +
+    "\n\n" +
+    "+ [<%= title %>](<%= pr.html_url %>) (#<%= pr.number %>)",
+  prReopenedCommentTemplate:
+    "<%= sender.login %>さんがプルリクエストを作成しました" +
+    "\n\n" +
+    "+ [<%= title %>](<%= pr.html_url %>) (#<%= pr.number %>)",
+  prReadyForReviewCommentTemplate:
+    "<%= sender.login %>さんがプルリクエストを作成しました" +
+    "\n\n" +
+    "+ [<%= title %>](<%= pr.html_url %>) (#<%= pr.number %>)",
+  prClosedCommentTemplate:
+    "<%= sender.login %>さんがプルリクエストをクローズしました" +
+    "\n\n" +
+    "+ [<%= title %>](<%= pr.html_url %>) (#<%= pr.number %>)",
+  prMergedCommentTemplate:
+    "<%= sender.login %>さんがプルリクエストをマージしました" +
+    "\n\n" +
+    "+ [<%= title %>](<%= pr.html_url %>) (#<%= pr.number %>)",
+  commitMessageRegTemplate:
+    "^" +
+    "(<%= projectKey %>\\-\\d+)\\s?" +
+    "(.*?)?\\s?" +
+    "(<% print(fixKeywords.join('|')) %>|<% print(closeKeywords.join('|')) %>)?" +
+    "$",
+  prTitleRegTemplate:
+    "^" +
+    "(<%= projectKey %>\\-\\d+)\\s?" +
+    "(.*?)?\\s?" +
+    "(<% print(fixKeywords.join('|')) %>|<% print(closeKeywords.join('|')) %>)?" +
+    "$",
+  fixStatusId: "3",
+  closeStatusId: "4",
+}
+
 /**
  * Parses and validations the action configuration
  * @returns Parsed the action configuration
@@ -31,59 +83,34 @@ export const getConfigs = (): Configs => {
     githubEventPath: getConfig("github_event_path", { required: true }),
     fixKeywords: getConfig("fix_keywords")
       ? getMultilineInput("fix_keywords")
-      : ["#fix", "#fixes", "#fixed"],
+      : defaultConfigs.fixKeywords,
     closeKeywords: getConfig("close_keywords")
       ? getMultilineInput("close_keywords")
-      : ["#close", "#closes", "#closed"],
+      : defaultConfigs.closeKeywords,
     pushCommentTemplate:
-      getConfig("push_comment_template") ||
-      "<%= commits[0].author.name %>さんが[<%= ref.name %>](<%= ref.url %>)にプッシュしました" +
-        "\n" +
-        "<% commits.forEach(commit=>{ %>" +
-        "\n" +
-        "+ [<%= commit.comment %>](<%= commit.url %>) (<% print(commit.id.slice(0, 7)) %>)" +
-        "<% }); %>",
+      getConfig("push_comment_template") || defaultConfigs.pushCommentTemplate,
     prOpenedCommentTemplate:
       getConfig("pr_opened_comment_template") ||
-      "<%= sender.login %>さんがプルリクエストを作成しました" +
-        "\n\n" +
-        "+ [<%= title %>](<%= pr.html_url %>) (#<%= pr.number %>)",
+      defaultConfigs.prOpenedCommentTemplate,
     prReopenedCommentTemplate:
       getConfig("pr_reopened_comment_template") ||
-      "<%= sender.login %>さんがプルリクエストを作成しました" +
-        "\n\n" +
-        "+ [<%= title %>](<%= pr.html_url %>) (#<%= pr.number %>)",
+      defaultConfigs.prReopenedCommentTemplate,
     prReadyForReviewCommentTemplate:
       getConfig("pr_ready_for_review_comment_template") ||
-      "<%= sender.login %>さんがプルリクエストを作成しました" +
-        "\n\n" +
-        "+ [<%= title %>](<%= pr.html_url %>) (#<%= pr.number %>)",
+      defaultConfigs.prReadyForReviewCommentTemplate,
     prClosedCommentTemplate:
       getConfig("pr_closed_comment_template") ||
-      "<%= sender.login %>さんがプルリクエストをクローズしました" +
-        "\n\n" +
-        "+ [<%= title %>](<%= pr.html_url %>) (#<%= pr.number %>)",
+      defaultConfigs.prClosedCommentTemplate,
     prMergedCommentTemplate:
       getConfig("pr_merged_comment_template") ||
-      "<%= sender.login %>さんがプルリクエストをマージしました" +
-        "\n\n" +
-        "+ [<%= title %>](<%= pr.html_url %>) (#<%= pr.number %>)",
+      defaultConfigs.prMergedCommentTemplate,
     commitMessageRegTemplate:
       getConfig("commit_message_reg_template") ||
-      "^" +
-        "(<%= projectKey %>\\-\\d+)\\s?" +
-        "(.*?)?\\s?" +
-        "(<% print(fixKeywords.join('|')) %>|<% print(closeKeywords.join('|')) %>)?" +
-        "$",
+      defaultConfigs.commitMessageRegTemplate,
     prTitleRegTemplate:
-      getConfig("pr_title_reg_template") ||
-      "^" +
-        "(<%= projectKey %>\\-\\d+)\\s?" +
-        "(.*?)?\\s?" +
-        "(<% print(fixKeywords.join('|')) %>|<% print(closeKeywords.join('|')) %>)?" +
-        "$",
-    fixStatusId: getConfig("fix_status_id") || "3",
-    closeStatusId: getConfig("close_status_id") || "4",
+      getConfig("pr_title_reg_template") || defaultConfigs.prTitleRegTemplate,
+    fixStatusId: getConfig("fix_status_id") || defaultConfigs.fixStatusId,
+    closeStatusId: getConfig("close_status_id") || defaultConfigs.closeStatusId,
   }
 }
 
